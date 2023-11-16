fb6df9b4bc7c6c9f8c21ceb89012c6b3304547d1
yarkincaner <yarkincaner@gmail.com>
2023-11-16 10:37:44 +0300

    feat: add explanation of Quicksort with Hoare's Partition

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -3,6 +3,28 @@ package sorting;
 import utils.Swap;
 
 public class QuickSort {
+    /**
+     * Quicksort with hoare's partition splits array into two pieces from q. Since sizes of these pieces are unknown,
+     * it is hard to assume a complexity for quicksort with hoare's partition.
+     *
+     * In a scenario where array is sorted; hoare's partition always returns the first index. That would result two
+     * recursive calls which one of them has size 1 and the other size n-1.
+     * That would result in -> T(n) = T(1) + T(n-1) + O(n) and the complexity would be O(n^2).
+     * For reversed-sorted input, it would be the opposite as T(n) = T(n-1) + T(1) + O(n); which results in O(n^2)
+     * Thus, sorted and reversed-sorted inputs gives us the worst cases as O(n^2).
+     *
+     * However, we want the average case. The probability of hoare's partition to not select the q index from the first %10
+     * and from te last %10 is %80. So if we assume that each recursive call divides array into %10 to %90 for the average case.
+     * This gives us T(n) = T(n/10) + T(9n/10) + O(n) which results in O(nlogn).
+     *
+     * Therefore, average case for Quicksort with Hoare's Partition gives us the complexity for;
+     * Worst case: O(n^2)
+     * Best and Average case: O(nlogn)
+     *
+     * @param A input array
+     * @param p first index
+     * @param r last index
+     */
     public static void hoare_sort(int[] A, int p, int r) {
         if (p >= r) return;
         int q = hoare_partition(A, p, r);
@@ -10,6 +32,18 @@ public class QuickSort {
         hoare_sort(A, q+1, r);
     }
 
+    /**
+     * Hoare's partition selects pivot value from the first index. i begins increasing from the first index until the
+     * value is less than pivot, j begins decreasing from the last index until the value is larger than pivot.
+     *
+     * Complexity: O(n) since it visits every index on input array. There are no worst or best cases.
+     *
+     * @param A input array
+     * @param p first index
+     * @param r last index
+     * @return a number which indicates left from that index is less than pivot, right from that is index is
+     * larger than pivot
+     */
     private static int hoare_partition(int[] A, int p, int r) {
         int pivot = A[p];
         int i = p-1;
